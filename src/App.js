f29e3bba5658909bde3fb81d2ac942ba09c19e9d
João Pedro <joaopetrussousa@gmail.com>
2022-07-01 15:12:20 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Album from './Components/Album';
+import Favorites from './Components/Favorites';
+import Login from './Components/Login';
+import Profile from './Components/Profile';
+import Search from './Components/Search';
+import ProfileEdit from './Components/ProfileEdit';
+import NotFound from './Components/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div className="Pages-Container">
+        <Route path="/" exact>
+          <Login />
+        </Route>
+
+        <Route path="/search">
+          <Search />
+        </Route>
+
+        <Route path="/album/:id" component={ Album } />
+
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+
+        <Route path="/profile">
+          <Profile />
+        </Route>
+
+        <Route path="/profile/edit">
+          <ProfileEdit />
+        </Route>
+
+        <Route path="">
+          <NotFound />
+        </Route>
+      </div>
+
+    );
   }
 }
 
