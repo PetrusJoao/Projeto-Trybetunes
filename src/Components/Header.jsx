673fa09174c82f3d14d9ed431438bef73736326a
João Pedro <joaopetrussousa@gmail.com>
2022-07-04 14:59:45 -0300

    Requisito 4

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -34,6 +35,12 @@ render() {
             User:
             {' '}
             { user }
+            <menu>
+              <Link to="/search" data-testid="link-to-search" />
+              <Link to="/favorites" data-testid="link-to-favorites" />
+              <Link to="/profile" data-testid="link-to-profile" />
+            </menu>
+
           </div>
         )}
 
